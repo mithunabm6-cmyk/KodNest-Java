5b867627ed1290567f358972c2b7268da741bf9a
Mithuna Bm <mithunabm6@gmail.com>
2026-08-14 11:42:23 +0530

    StudentApplication2

---
+++ track/M02-Introduction-to-java/StudentApplication2.java
@@ -0,0 +1,28 @@
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    void input(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
+
+class StudentApplication {
+
+    public static void main(String[] args) {
+        {
+            Student s1 = new Student("Raja, 19 , 4.6");
+        }
+    }
+}
